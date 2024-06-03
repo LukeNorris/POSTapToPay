f3db5e7dbb07be14898bae9fd5dd37c0aea42c0a
Luke Norris <luke.norris@adyen.com>
2024-06-03 13:37:18 -0600

    added a toast for boarding button is clicked

--- app/src/main/java/com/adyen/postaptopay/presentation/viewmodels/BoardingViewModel.kt
+++ app/src/main/java/com/adyen/postaptopay/presentation/viewmodels/BoardingViewModel.kt
@@ -6,12 +6,14 @@ import android.net.Uri
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.lifecycle.AndroidViewModel
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.adyen.postaptopay.data.local.InstallationIdRepository
 import com.adyen.postaptopay.data.remote.BoardingRepository
 import com.adyen.postaptopay.data.remote.BoardingResponse
+import com.adyen.postaptopay.util.DeepLinkUtils
+import com.adyen.postaptopay.util.ToastUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,140 +21,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.URLEncoder
 import java.util.Base64
-
-/*class BoardingViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val repository = BoardingRepository()
-    private val installationIdRepository = InstallationIdRepository(application)
-
-    private val _apiResponse = MutableStateFlow<String?>(null)
-    val apiResponse: StateFlow<String?> = _apiResponse
-
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> = _isLoading
-
-    private val _error = MutableStateFlow<String?>(null)
-    val error: StateFlow<String?> = _error
-
-    fun checkBoardingStatus(activity: AppCompatActivity) {
-        viewModelScope.launch {
-            val installationId = withContext(Dispatchers.IO) {
-                installationIdRepository.getInstallationId()
-            }
-            Log.d("BoardingViewModel", "Current Installation ID: $installationId")
-            if (installationId.isNullOrEmpty()) {
-                Log.d("BoardingViewModel", "No installation ID found. Initiating boarding process.")
-                initiateBoarding(activity)
-            } else {
-                Log.d("BoardingViewModel", "App is already boarded. Installation ID: $installationId")
-            }
-        }
-    }
-
-    private fun initiateBoarding(activity: AppCompatActivity) {
-        val returnUrl = "merchantscheme://com.merchant.companion/onboarding"
-        val returnUrlEncoded = URLEncoder.encode(returnUrl, Charsets.UTF_8.name())
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            data = Uri.parse("adyenpayments://boarded?returnUrl=$returnUrlEncoded")
-        }
-        openDeepLink(activity, intent)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun handleDeepLinkResponse(boardingRequestToken: String?, boarded: String?, installationId: String?, activity: AppCompatActivity) {
-        Log.d("BoardingViewModel", "Handling deep link response. BoardingRequestToken: $boardingRequestToken, InstallationId: $installationId, Boarded: $boarded")
-        if (boarded == "true") {
-            Log.d("BoardingViewModel", "App is already boarded. Boarded parameter in URL is true")
-            if (installationId != null) {
-                saveInstallationId(installationId)
-            } else {
-                Log.e("BoardingViewModel", "Received boarding status as true but no installation ID.")
-                _error.value = "Installation ID is missing despite boarded being true."
-            }
-        } else if (boarded == "false") {
-            if (boardingRequestToken != null) {
-                generateBoardingToken(boardingRequestToken, activity)
-                Log.d("BoardingViewModel", "generateBoardingToken called")
-            } else {
-                Log.e("BoardingViewModel", "Boarding request token is null.")
-                _error.value = "Boarding request token is null."
-            }
-        } else {
-            Log.e("BoardingViewModel", "Unhandled deep link response.")
-            _error.value = "Unhandled deep link response."
-        }
-    }
-
-    private fun saveInstallationId(installationId: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            Log.d("BoardingViewModel", "Saving Installation ID: $installationId")
-            installationIdRepository.setInstallationId(installationId)
-            Log.d("BoardingViewModel", "Installation ID saved: $installationId")
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun generateBoardingToken(boardingRequestToken: String, activity: AppCompatActivity) {
-        viewModelScope.launch {
-            Log.d("BoardingViewModel", "Starting to generate boarding token.")
-            _isLoading.value = true
-            _error.value = null
-
-            try {
-                Log.d("BoardingViewModel", "Sending network request to generate boarding token")
-                val response: BoardingResponse? = withContext(Dispatchers.IO) {
-                    repository.generateBoardingToken(boardingRequestToken)
-                }
-                Log.d("BoardingViewModel", "Network request completed with response: $response")
-                response?.let {
-                    Log.d("BoardingViewModel", "Boarding token generated successfully: ${it.boardingToken}")
-                    _apiResponse.value = it.boardingToken
-                    sendBoardingTokenToApp(it.boardingToken, activity)
-                } ?: run {
-                    Log.e("BoardingViewModel", "Failed to generate boarding token.")
-                    _error.value = "Failed to generate boarding token"
-                }
-            } catch (e: Exception) {
-                Log.e("BoardingViewModel", "Error generating boarding token", e)
-                _error.value = e.localizedMessage
-                _apiResponse.value = null
-            } finally {
-                _isLoading.value = false
-                Log.d("BoardingViewModel", "Finished generating boarding token.")
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun sendBoardingTokenToApp(boardingToken: String, activity: AppCompatActivity) {
-        Log.d("BoardingViewModel", "Sending boarding token to app")
-        val returnUrl = "merchantscheme://com.merchant.companion/onboarding"
-        val returnUrlEncoded = URLEncoder.encode(returnUrl, Charsets.UTF_8.name())
-        val boardingTokenBase64 = Base64.getEncoder().encodeToString(boardingToken.toByteArray())
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            data = Uri.parse("adyenpayments://board?boardingToken=$boardingTokenBase64&returnUrl=$returnUrlEncoded")
-        }
-        openDeepLink(activity, intent)
-    }
-
-    fun openDeepLink(activity: AppCompatActivity, intent: Intent) {
-        try {
-            Log.d("BoardingViewModel", "Opening deep link: ${intent.data}")
-            if (intent.resolveActivity(activity.packageManager) != null) {
-                activity.startActivity(intent)
-                Log.d("BoardingViewModel", "Deep link opened successfully")
-            } else {
-                Log.e("BoardingViewModel", "No activity found to handle deep link: ${intent.data}")
-            }
-        } catch (e: Exception) {
-            Log.e("BoardingViewModel", "Error opening deep link", e)
-        }
-    }
-}*/
-
-
-
-import com.adyen.postaptopay.util.DeepLinkUtils
 
 class BoardingViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -179,6 +47,7 @@ class BoardingViewModel(application: Application) : AndroidViewModel(application
                 initiateBoarding(activity)
             } else {
                 Log.d("BoardingViewModel", "App is already boarded. Installation ID: $installationId")
+                ToastUtils.showToast(activity, "App is already boarded. Installation ID: $installationId")
             }
         }
     }
@@ -198,9 +67,11 @@ class BoardingViewModel(application: Application) : AndroidViewModel(application
                 Log.d("BoardingViewModel", "App is already boarded. Boarded parameter in URL is true")
                 if (installationId != null) {
                     saveInstallationId(installationId)
+                    ToastUtils.showToast(activity, "App is already boarded. Installation ID: $installationId")
                 } else {
                     Log.e("BoardingViewModel", "Received boarding status as true but no installation ID.")
                     _error.value = "Installation ID is missing despite boarded being true."
+                    ToastUtils.showToast(activity, "Installation ID is missing despite boarded being true.")
                 }
             }
             "false" -> {
@@ -210,11 +81,13 @@ class BoardingViewModel(application: Application) : AndroidViewModel(application
                 } else {
                     Log.e("BoardingViewModel", "Boarding request token is null.")
                     _error.value = "Boarding request token is null."
+                    ToastUtils.showToast(activity, "Boarding request token is null.")
                 }
             }
             else -> {
                 Log.e("BoardingViewModel", "Unhandled deep link response.")
                 _error.value = "Unhandled deep link response."
+                ToastUtils.showToast(activity, "Unhandled deep link response.")
             }
         }
     }
@@ -247,11 +120,13 @@ class BoardingViewModel(application: Application) : AndroidViewModel(application
                 } ?: run {
                     Log.e("BoardingViewModel", "Failed to generate boarding token.")
                     _error.value = "Failed to generate boarding token"
+                    ToastUtils.showToast(activity, "Failed to generate boarding token")
                 }
             } catch (e: Exception) {
                 Log.e("BoardingViewModel", "Error generating boarding token", e)
                 _error.value = e.localizedMessage
                 _apiResponse.value = null
+                ToastUtils.showToast(activity, "Error generating boarding token: ${e.localizedMessage}")
             } finally {
                 _isLoading.value = false
                 Log.d("BoardingViewModel", "Finished generating boarding token.")
@@ -271,4 +146,3 @@ class BoardingViewModel(application: Application) : AndroidViewModel(application
         DeepLinkUtils.openDeepLink(activity, intent.data!!)
     }
 }
-
